ebdcaa05cb58f2f77af7dc9a09e547c836f4d861
Shelby <shelby.harrison6@hotmail.com>
2022-03-06 22:32:55 +0000

    button fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Home.css';
 import homeImg from '../images/pexels-marianne-238385.jpg';
 import famImg from '../images/pexels-vidal-balielo-jr-1682497.jpg';
@@ -7,15 +8,13 @@ const Home = () => (
   <div className="homepage">
     <div className="homepage-box">
       <p className="homepage-text">Search properties for sale in the UK</p>
-      <button className="view-button"><a href="/view-properties"></a>
-Search
-</button>
+      <Link to="/view-properties" historyType="replace"><button type="button" className="view-button">Search</button></Link>
       <img className="homeImg" src={homeImg} alt="house" />
     </div>
     <div className="homepage-secondbox">
       <h2 className="home-h2">Ready to move on?</h2>
       <p className="homepage-secondtext">Add your property to the market now with our simple form!</p>
-      <button className="add-link-button"><a href="/add-property"></a>Add</button>
+      <Link to="/add-property"><button type="button" className="add-link-button">Add</button></Link>
       <img className="famImg" src={famImg} alt="family" />
     </div>
   </div>
